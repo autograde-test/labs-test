fcb2c72235cd7fe40d8e6717b1be8d523ce7e338
FakeHeine <fakeheine@gmail.com>
2015-08-20 21:55:31 +0200

    Update test.sh

--- lab1/test.sh
+++ lab1/test.sh
@@ -2,3 +2,4 @@
 # This is a comment!
 echo "---PASS"
 echo "---FAIL"
+echo "{\"Secret\":\"cd7af588070f1c6579b1fca35f993125\",\"TestName\":\"Test Case 1\",\"Score\":50,\"MaxScore\":100,\"Weight\":1}"
