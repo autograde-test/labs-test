e94dd1ac186aadf11932842db6ed58731546bb3b
Heine Furubotten <hfurubotten@users.noreply.github.com>
2015-08-24 19:15:08 +0200

    Update test.sh

--- lab1/test.sh
+++ lab1/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This is a comment!
-echo "---PASS"
-echo "---FAIL"
+echo "--- PASS"
+echo "--- FAIL"
 echo "{\"Secret\":\"cd7af588070f1c6579b1fca35f993125\",\"TestName\":\"Test Case 1\",\"Score\":50,\"MaxScore\":100,\"Weight\":1}"
